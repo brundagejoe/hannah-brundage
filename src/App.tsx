2c4adf1c363b6458d372d2d98c283ed6a303ffb8
brundagejoe <joebrundage@icloud.com>
2023-07-02 08:11:11 -0600

    changed wording

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,22 @@ const allItems: Item[] = [
   { id: 3, name: "Thirty minute cuddle session", used: false },
   { id: 4, name: "Thirty minute cuddle session", used: false },
   { id: 5, name: "Thirty minute cuddle session", used: false },
+  { id: 6, name: "One trip to make candles", used: false },
+  { id: 7, name: "One dinner where we BOTH eat salads", used: false },
+  { id: 8, name: "One weekend camping", used: false },
+  { id: 9, name: "One dessert where we BOTH eat dessert", used: false },
+  { id: 10, name: "One dessert where we BOTH eat dessert", used: false },
+  { id: 11, name: "One dessert where we BOTH eat dessert", used: false },
+  { id: 12, name: "One massage from ME", used: false },
+  { id: 13, name: "One massage from ME", used: false },
+  { id: 14, name: "One massage from ME", used: false },
+  { id: 15, name: "One massage from a PROFESSIONAL", used: false },
+  {
+    id: 16,
+    name: "One day without ANY technology (except our car?)",
+    used: false,
+  },
+  { id: 17, name: "One party for DAISY", used: false },
 ];
 const App = () => {
   const [items, setItems] = useState(allItems.sort(() => Math.random() - 0.5));
@@ -37,7 +53,7 @@ const App = () => {
         ❤️
       </h1>
       <div className="mt-60 px-2">
-        <div>Your coupons:</div>
+        <div>Your starving Hannah card:</div>
         <div className="text-sm">(To be redeemed within one year!)</div>
         {items.map((item) => (
           <div
